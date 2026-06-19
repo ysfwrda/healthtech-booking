763d50ca0bc7fca7ec05400272e8fcb1191e4642
Youssef Abouwarda <ysfwrda@gmail.com>
2026-06-20 01:03:27 +0200

    chore: add appointment-service Spring Boot project scaffold

---
+++ appointment-service/src/test/java/com/healthtech/appointment/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthtech.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
