3417074da8df418395a1274428105228439b7c47
Youssef Abouwarda <ysfwrda@gmail.com>
2026-06-27 17:24:45 +0200

    test: add slice tests for patient profile ownership check

--- patient-service/src/main/java/com/healthtech/patient/dto/PatientResponse.java
+++ patient-service/src/main/java/com/healthtech/patient/dto/PatientResponse.java
@@ -1,10 +1,7 @@
 package com.healthtech.patient.dto;
 
 import com.healthtech.patient.domain.InsuranceType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,6 +11,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class PatientResponse {
     private UUID id;
     private String firstName;
